554ec462219dade2a0358135c3addc07fd0b38bd
choiseongjun <csj2702@naver.com>
2025-05-18 11:20:21 +0900

    feat: 상품 및 결제 서버 추가

---
+++ goods-system/src/main/java/com/ts/goodssystem/GoodsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ts.goodssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsSystemApplication.class, args);
+    }
+
+}
---
+++ goods-system/src/test/java/com/ts/goodssystem/GoodsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.goodssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
